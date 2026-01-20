fc01280c019b92f2ff6fbf0a83541ca3dce3d44c
TBall <TBall>
2026-01-20 18:46:24 -0500

    demo1

---
+++ Demo.1/sketch.js
@@ -0,0 +1,36 @@
+let size = 30;
+let cols = 20;
+let rows = 20;
+
+let color_speed=0.3;
+
+function setup() {
+  createCanvas(cols * size, rows * size);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(255);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * size + size / 2;
+      let y = j * size + size / 2;
+      let d = dist(x, y, mouseX, mouseY);
+      let angle = d * 0.01;
+
+      push();
+      translate(x, y);
+      rotate(angle);
+      fill((d * color_speed) % 255, 200 - ((d * color_speed)% 100), 100);
+      stroke(255 - ((d * color_speed) % 255), 200, (d * color_speed) % 200);
+      strokeWeight(6);
+      rect(0, 0, size - 4, size - 4);
+      pop();
+    }
+  }
+}
+
+function touchMoved() {
+  return false;
+}
