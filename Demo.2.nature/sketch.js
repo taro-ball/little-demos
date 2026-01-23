78dbf8ee69074caa04e93721ebf97d6faf97efc7
TBall <TBall>
2026-01-23 18:28:55 -0500

    spiral length

--- Demo.2.nature/sketch.js
+++ Demo.2.nature/sketch.js
@@ -1,5 +1,8 @@
 function setup() {
   createCanvas(400, 400);
+}
+
+function draw() {
   background(255);
   stroke(34, 139, 34);
   strokeWeight(2);
@@ -7,8 +10,10 @@ function setup() {
 
   translate(150, 350);
 
+  let end = map(mouseY, 0, height, 0, 8);
+
   beginShape();
-  for (let a = 0; a < 8; a += 0.05) {
+  for (let a = 8; a > end; a -= 0.05) {
     let r = 5 + a * 8;
     let x = r * cos(a + PI);
     let y = -r * sin(a + PI);
