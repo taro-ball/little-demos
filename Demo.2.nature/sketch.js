abd48aa46fab568f8ec04c62e721edc75eebe9a6
TBall <TBall>
2026-01-24 00:06:37 -0500

    grow from stalk

--- Demo.2.nature/sketch.js
+++ Demo.2.nature/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
   createCanvas(900, 900);
   segmentSize = 0.1;
-  insideDiameter = 0;
+  insideDiameter = 5;
   
 
 }
@@ -12,11 +12,17 @@ function draw() {
   strokeWeight(2);
   noFill();
 
-  translate(150, 350);
-
   // let end = map(mouseY, 0, height, 0, 8);
   let end = 0;
   let scale2 = map(mouseX, 0, width, 0.5, 30);
+
+  // Calculate tail position (at a=8) and translate so it stays fixed
+  let tailA = 8;
+  let tailR = insideDiameter + tailA * scale2;
+  let tailX = tailR * cos(tailA);
+  let tailY = tailR * sin(tailA);
+
+  translate(150 - tailX, 350 - tailY);
 
   beginShape();
   for (let a = 8; a > end; a -= segmentSize) {
